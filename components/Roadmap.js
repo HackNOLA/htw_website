40eceaf6e51268b166624f39aed0b1ba29a0bbf4
Akintunde Salako Pounds <akinpds@gmail.com>
2021-11-16 16:13:40 -0600

    mld intro

--- components/Roadmap.js
+++ components/Roadmap.js
@@ -20,7 +20,7 @@ return (
   >
     <h4 className="vertical-timeline-element-title">Launch our Token on the <span className="text-primary font-weight-medium">Harmony</span> network</h4>
     <p>
-      We plan to utilize the Harmony network for our token. We will be launching our token on the Harmony network in the near future. Our token will allow us to raise funds for our project. It will also help us to build a community of developers and designers.
+      We plan to utilize the <a className="text-primary font-weight-medium" href="https://www.harmony.one/">Harmony network</a> for our token. We will be launching our token on the Harmony network in the near future. Our token will allow us to raise funds for our project. It will also help us to build a community of developers and designers.
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
@@ -50,7 +50,17 @@ return (
   >
     <h4 className="vertical-timeline-element-title">Intergrate our <span className="text-primary font-weight-medium">Web 3.0</span> course</h4>
     <p>
-        Our plan is to teach the software development skills necessary to build a decentralized application using the Ethereum blockchain. 
+        Our plan is for HackNOLA to teach the software development skills necessary to build a decentralized application using the Ethereum blockchain. 
+    </p>
+  </VerticalTimelineElement>
+  <VerticalTimelineElement
+    className="vertical-timeline-element--work"
+    date="February, 2022"
+    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+  >
+    <h4 className="vertical-timeline-element-title">Launch <span className="text-primary font-weight-medium">My Little Dino</span></h4>
+    <p>
+        Amongst our educational projects, we are releasing our first game, <a className="text-primary font-weight-medium" href="https://my-little-dino.gitbook.io/my-little-dino/">My Little Dino</a>. My Little Dino is a multiplayer, play-to-earn game that will be available on the Harmony network.
     </p>
   </VerticalTimelineElement>
   <VerticalTimelineElement
